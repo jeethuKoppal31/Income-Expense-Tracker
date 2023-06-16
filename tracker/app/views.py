735613326d4730d06e24fb8048c20af8618c0002
Amrutha Raj <amrutha42000@gmail.com>
2023-06-16 19:44:06 +0530

    Implement login page and logout page

--- tracker/app/views.py
+++ tracker/app/views.py
@@ -1,27 +1,45 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+# Create your views here.
+@login_required(login_url='login')
+def HomePage(request):
+    return render (request,'home.html')
 
 def SignupPage(request):
-    if request.method == 'POST':
-        uname = request.POST.get('username')
-        email = request.POST.get('email')
-        pass1 = request.POST.get('password1')
-        pass2 = request.POST.get('password2')
-        
-        if pass1 != pass2:
-            return HttpResponse("Your password and confirm password are not the same!")
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
         else:
-            
-            try:
-                user = User.objects.create_user(username=uname, email=email, password=pass1)
-                
-                return HttpResponse("User created successfully!")
-            except Exception as e:
-                return HttpResponse(f"Error creating user: {str(e)}")
-    else:
-        return render(request, 'signup.html') 
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            messages.success(request, "Account created successfully. You can now login.")
+            return redirect('login')
+        
+
+
+
+    return render (request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
